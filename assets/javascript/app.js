a4585f1a470a9833ca0a6c5aad16eea24a7f228d
Monica Szabo <monicaszb@gmail.com>
2016-05-31 22:01:18 -0500

    all working, need to add styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,56 +1,70 @@
 $(document).ready(function() {
-	var gifArray = ["happy", "sad", "ambivalent", "angry", "surprised",];
+	var gifArray = ["happy", "sad", "ambivalent", "angry", "surprised", "despondent", "tired"];
 
 
 	function buttonCreator() {
+		$('#buttonPlace').empty();
+
 		for(var i = 0; i < gifArray.length; i++) {
 			$('#buttonPlace').append("<button class='emotions' data-emotion=" + gifArray[i] + ">" + gifArray[i] + "</button>");
 		}
 	}
 
 	function onClickEmotions() {
-		$(".emotions").on('click', function() {
-			var emo = $(this).data('emotion');
-			//var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + emo + "&limit=10";
+		var emo = $(this).attr('data-emotion');
+		//var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + emo + "&limit=10";
 
-			var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + emo + "&api_key=dc6zaTOxFJmzC&limit=10";
-			
-			$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-				var results = response.data;
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + emo + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-                for (var i = 0; i < results.length; i++) {
-                    if(results[i].rating == "r") {
+		$('#gifPlace').empty();
+		
+		$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+			var results = response.data;
 
-                    }
-                    else {
-                        var gifDiv = $('<div class="gif">')
-                        var ratingText = $('<p>').text("Rating: " + results[i].rating);
-                        var emoImage = $('<img src=' + results[i].images.fixed_height.url + '>');
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $('<div class="gif">')
+                var ratingText = $('<p>').text("Rating: " + results[i].rating);
+                var emoImage = $('<img src=' + results[i].images.fixed_height_still.url + ' data-still=' +
+                	results[i].images.fixed_height_still.url + ' data-animate=' +
+                	results[i].images.fixed_height.url + ' data-state="still" class="emoImage">');
 
-                        gifDiv.append(ratingText);
-                        gifDiv.append(emoImage);
+                gifDiv.append(ratingText);
+                gifDiv.append(emoImage);
 
-                        $('#gifPlace').prepend(gifDiv);
-                    }
-                }
-			});
+                $('#gifPlace').prepend(gifDiv);
+            }
+
 		});
 	}
 
 	function addEmotion() {
 		$('#addEmotion').on('click', function(){
 			var typed = $('#emotion-input').val().trim();
-
 			gifArray.push(typed);
-
-			$('#buttonPlace').empty();
 			buttonCreator();
 			return false;
-		})		
+		})
 	}
 
-	buttonCreator();
-	onClickEmotions();
-	addEmotion();
+	function imageMove() {
+		var state = $(this).attr('data-state');
 
+	    if(state == 'still') {
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        }
+        else {
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
+	}
+
+
+	$(document).on('click', '.emotions', onClickEmotions);
+
+
+	$(document).on('click', '.emoImage', imageMove);
+
+	buttonCreator();
+	addEmotion();
 });
